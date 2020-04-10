fee1b1d6d612cb4324863951d66ab8168a286ed9
yashsaboo <yashsaboo99@gmail.com>
2020-04-09 21:32:00 -0500

    CSV updated with new data from Xin, DBtoWebsite done and Website toDB Insert done

---
+++ Src/Convert_CSV/DatabaseHelperFunctions.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+import MySQLdb as mdb
+
+
+# In[16]:
+
+
+DBNAME = "dbtest"
+DBHOST = "localhost"
+DBPASS = ""
+DBUSER = "root"
+
+
+# In[17]:
+
+
+def connectToDatabase():
+    try:
+        db = mdb.connect(DBHOST, DBUSER, DBPASS, DBNAME, charset='utf8', port=3308)
+        print("Database Connected Successfully")
+        return db
+    except mdb.Error as e:
+        print(e)
+        print("Database Not Connected Successfully")
+        return None
+    
+def closeDatabase(db):
+    try:
+        db.close()
+        print("Database Closed Successfully")
+    except mdb.Error as e:
+        print(e)
+        print("Database Not Closed Successfully")
+
+
+# In[21]:
+
+
+def executeSingleQuery(sqlquery):
+    
+    db = connectToDatabase()
+    
+    try:
+        cur = db.cursor()
+
+        # execute query
+        cur.execute(sqlquery)
+        print("Query Successfully Executed")
+        
+        db.commit()
+
+    except mdb.Error as e:
+        print(e)
+        print("Query Not Successfully Executed" + sqlquery)
+        
+    closeDatabase(db)
+    
+def executeSingleQueryWhichReturns(sqlquery):
+    
+    db = connectToDatabase()
+    
+    try:
+        cur = db.cursor()
+
+        # execute query
+        number_of_rows = cur.execute(sqlquery)
+        result = cur.fetchall()
+        print("Query Successfully Executed and Fetched")
+        
+        db.commit()
+
+    except mdb.Error as e:
+        print(e)
+        print("Query Not Successfully Executed and Fetched" + sqlquery)
+        
+    closeDatabase(db)
+    
+    return result
+
+
+# In[ ]:
+
+
+
+
